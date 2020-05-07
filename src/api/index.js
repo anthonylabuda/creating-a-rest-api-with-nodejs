63e2a7d54c2e738f40a32f56739f04e25a1e8fe5
Anthony LaBuda <anthony.j.labuda@gmail.com>
2020-05-06 22:17:41 -0500

    Added Morgan to Express API as a logging middleware;

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 import routes from "./routes";
 
 const api = express();
+
+api.use(morgan('dev'));
 
 api.use('/orders', routes.orders);
 api.use('/products', routes.products);
