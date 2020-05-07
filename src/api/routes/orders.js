45a81dad9bd0e2e7eb324afcdf3a7ed638719e68
Anthony LaBuda <anthony.j.labuda@gmail.com>
2020-05-06 22:51:41 -0500

    Integrated body parsing into routes;

--- src/api/routes/orders.js
+++ src/api/routes/orders.js
@@ -17,7 +17,10 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-    const order = {};
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
 
     res.status(201).json({
         "message": "Handling POST requests to /orders",
--- src/api/routes/products.js
+++ src/api/routes/products.js
@@ -17,7 +17,10 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-    const product = {};
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
 
     res.status(201).json({
         "message": "Handling POST requests to /products",
