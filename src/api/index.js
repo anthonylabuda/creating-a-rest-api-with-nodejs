b478193dc6ecab51203afbdaff2d101f1218d350
Anthony LaBuda <anthony.j.labuda@gmail.com>
2020-05-06 22:29:48 -0500

    4: Completed section; Added error handling for invalid resources;

--- src/api/index.js
+++ src/api/index.js
@@ -9,4 +9,19 @@ api.use(morgan('dev'));
 api.use('/orders', routes.orders);
 api.use('/products', routes.products);
 
+api.use((req, res, next) => {
+    const error = new Error('Not Found');
+
+    error.status = 404;
+
+    next(error);
+});
+
+api.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        "message": error.message
+    });
+});
+
 export default api;
