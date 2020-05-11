4643b2dde1efa6e533db2db68b8d645122a88aec
Anthony LaBuda <anthony.j.labuda@gmail.com>
2020-05-11 12:39:11 -0500

    12: Completed section; Added JWT creation on valid login;

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import express from "express";
+import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 import User from "../models/user.js";
 
@@ -18,6 +19,33 @@ router.delete(`/:_id`, (req, res, next) => {
 });
 
 // -------------------------
+// /users/login
+// -------------------------
+router.post(`/login`, (req, res, next) => {
+    const { email, password } = req.body;
+
+    User.find({ email })
+        .exec()
+        .then(user => {
+            if (user.length !== 1) return res.status(401).json({ user: { email } });
+
+            bcrypt.compare(password, user[0].password, (error, valid) => {
+                if (error) return res.status(500).json({ error });
+
+                if (valid) {
+                    const { _id, email } = user[0];
+                    const token = jwt.sign({ _id, email }, process.env.JWT_SECRET, { expiresIn: `1h` });
+
+                    return res.status(200).json({ token })
+                };
+
+                res.status(401).json({ user: { email } });
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
+});
+
+// -------------------------
 // /users/signup
 // -------------------------
 router.post(`/signup`, (req, res, next) => {
@@ -26,7 +54,7 @@ router.post(`/signup`, (req, res, next) => {
     User.find({ email })
         .exec()
         .then(user => {
-            if (user.length >= 1) return res.status(409).json({ user: { email } });
+            if (user.length !== 0) return res.status(409).json({ user: { email } });
 
             bcrypt.hash(password, 10, (error, hash) => {
                 if (error) return res.status(500).json({ error });
