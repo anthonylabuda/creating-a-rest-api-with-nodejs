b41b0c57e53554c474d7028cfb880ef5115e8746
Anthony LaBuda <anthony.j.labuda@gmail.com>
2020-05-11 18:45:41 -0500

    Added more middleware;

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -2,9 +2,11 @@ import compression from "compression";
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
+import hpp from "hpp";
 import mongoSanitize from "express-mongo-sanitize";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
+import xssClean from "xss-clean";
 
 import settings from "../settings.js";
 
@@ -14,7 +16,9 @@ export default (api) => {
     api.use(compression());
     api.use(cors())
     api.use(helmet());
+    api.use(hpp());
     api.use(mongoSanitize());
     api.use(morgan(settings.middleware.morgan.format));
     api.use(rateLimit(settings.middleware.rateLimit.options));
+    apo.use(xssClean());
 };
