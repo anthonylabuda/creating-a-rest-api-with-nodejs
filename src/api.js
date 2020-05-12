00b95025ec8ee3315bfc8dec11c80d112be097fe
Anthony LaBuda <anthony.j.labuda@gmail.com>
2020-05-11 19:06:40 -0500

    Renaming stuff...;

--- src/api.js
+++ src/api.js
@@ -1,15 +1,15 @@
 import express from "express";
 
 import settings from "./settings.js";
-import applyDatabase from "./utils/database.js";
-import applyMiddleware from "./utils/middleware.js";
-import applyRouter from "./utils/router.js";
+import initializeDatabase from "./utils/database.js";
+import initializeMiddleware from "./utils/middleware.js";
+import initializeRouter from "./utils/router.js";
 
 const api = express();
 
-applyDatabase();
-applyMiddleware(api);
-applyRouter(api);
+initializeDatabase();
+initializeMiddleware(api);
+initializeRouter(api);
 
 api.listen(settings.api.port, () => console.log(`[API] :: Listening on port: ${settings.api.port}`));
 
