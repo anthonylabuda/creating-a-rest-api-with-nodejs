ca09d8bf275229e7579284b5a9c3b193d2b87ab8
Anthony LaBuda <anthony.j.labuda@gmail.com>
2020-05-11 19:24:18 -0500

    Removed unnecessary paranthesis;

--- src/api.js
+++ src/api.js
@@ -13,12 +13,12 @@ initializeRouter(api);
 
 api.listen(settings.api.port, () => console.log(`[API] :: Listening on port: ${settings.api.port}`));
 
-process.on(`uncaughtException`, (error) => {
+process.on(`uncaughtException`, error => {
     console.error(`[EXCEPTION] :: An uncaught Exception occurred; Shutting down API...`, error);
     process.exit(1);
 });
 
-process.on(`unhandledRejection`, (error) => {
+process.on(`unhandledRejection`, error => {
     console.error(`[EXCEPTION] :: An unhandled Exception occurred; Shutting down API...`, error);
     server.close(() => process.exit(1));
 });
